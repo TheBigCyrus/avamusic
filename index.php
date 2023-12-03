135c6000738427350343f3bdd7707b03bfc129f3
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-12-03 20:57:17 +0330

    make index route,ctrl,view and create db conn

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+require_once __DIR__."/models/database.php";
+
+date_default_timezone_set('Asia/Tehran');
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request){
+    case '/avamusic/home':
+    case '/avamusic/':
+    case '/avamusic/index':
+        require __DIR__ ."/controllers/index.php";
+        break;
+
+    default:
+        die("404");
+}
+?>
